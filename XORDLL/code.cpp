61d842cb3601e190678f4089cf788138e6019342
ajaycs18 <ajay.cs18+lab@bmsce.ac.in>
2020-09-23 15:38:33 +0530

    add delete tail and head functions

--- XORDLL/code.cpp
+++ XORDLL/code.cpp
@@ -35,6 +35,29 @@ Node* insertBeg(Node* head, int val) {
   return node;
 }
 
+Node* deleteEnd(Node* head) {
+  if (!head) 
+    return NULL;
+  Node* prev = NULL, *curr = head, *next = XOR(prev, curr->npx);
+  while (next) {
+    prev = curr;
+    curr = next;
+    next = XOR(prev, curr->npx);
+  }
+  prev->npx = XOR(prev->npx, curr);
+  delete curr;
+  return head;
+}
+
+Node* deleteBeg(Node* head) {
+  if (!head) 
+    return NULL;
+  Node* next = XOR(head->npx, NULL);
+  next->npx = XOR(head, next->npx);
+  delete head;
+  return next;
+}
+
 void printList(Node* curr) {
   Node* prev = NULL;
   while (curr) {
@@ -67,6 +90,14 @@ int main() {
   }
   cout << "List: " << endl;
   printList(head); 
+
+  cout << "List after removing tail: " << endl;
+  head = deleteEnd(head);
+  printList(head);
+
+  cout << "List after removing head: " << endl;
+  head = deleteBeg(head);
+  printList(head);
 
   return 0;
 }
